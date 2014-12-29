9c60251cf7b304cb507eaf02e4fd3947185ef8ca
Douglas Berringer <Doug.Berringer@gmail.com>
2014-12-29 14:12:53 -0800

    Converted to application
    * Make todo list dynamicly generate view from static list of todos
    * Link up the todo delte button
    * Link up the new todo input and submit button

--- angular/app/scripts/controllers/main.js
+++ angular/app/scripts/controllers/main.js
@@ -8,10 +8,34 @@
  * Controller of the ngTodoMvcApp
  */
 angular.module('ngTodoMvcApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope) {
+		// Static list of todo items
+		$scope.todos = [
+			{
+				text: 'Paint fence',
+				done: false
+			},
+			{
+				text: 'Wash car',
+				done: false
+			},
+			{
+				text: 'Sand floor',
+				done: true
+			}
+		];
+
+		$scope.deleteTodo = function (index) {
+			$scope.todos.splice(index,1);
+		}
+
+		$scope.addTodo = function () {
+			$scope.todos.push({
+				text: $scope.newTodo,
+				done: false
+			});
+
+			$scope.newTodo = '';
+		}
+
+	});
